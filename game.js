0e8fa12d510818ff069ce9090bef8086d0ae8150
Lukas_Skywalker <lukas.diener@hotmail.com>
2015-05-04 21:32:07 +0200

    more things

--- game.js
+++ game.js
@@ -1,14 +1,43 @@
 function createEntity(element) {
     return {
         element: element,
+        game: null,
         richtung: 0,
         geschwindigkeit: 1,
         speed: function() { return this.geschwindigkeit; },
         positionX: function() { return this.element.offset().left; },
         positionY: function() { return this.element.offset().top; },
-        setPosition: function(x, y) { this.element.css({left: x, top: y }); },
-        move: function() { this.element.css({left: this.positionX() + cos2(this.richtung) * this.speed(), top: this.positionY() - sin2(this.richtung) * this.speed() }); }
+        height: function() { return this.element.height(); },
+        width: function() { return this.element.width(); },
+        setPosition: function(x, y) { this.element.css({left: x + this.game.left(), top: y + this.game.top() }); },
+        move: function() { this.element.css({left: this.positionX() + cos2(this.richtung) * this.speed(), top: this.positionY() - sin2(this.richtung) * this.speed() }); },
+      collision: function(b) {
+        return !(
+          ((this.positionY() + this.height()) < (b.positionY())) ||
+          (this.positionY() > (b.positionY() + b.height())) ||
+          ((this.positionX() + this.width()) < b.positionX()) ||
+          (this.positionX() > (b.positionX() + b.width()))
+        );
+      }
     };
+}
+
+function createGame(element) {
+  return {
+    element: element,
+    entities: [],
+    add: function(entity) {
+      this.entities.push(entity);
+      entity.game = this;
+      this.element.append(entity.element);
+    },
+    top: function() {
+      return this.element.offset().top;
+    },
+    left: function() {
+      return this.element.offset().left;
+    }
+  };
 }
 
 function sin2(deg) {
