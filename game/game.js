20db9339f2e7e7575895fe66fdefc89069805a38
Lukas_Skywalker <lukas.diener@hotmail.com>
2015-11-10 14:41:12 +0100

    update game & 13

--- game/game.js
+++ game/game.js
@@ -1,4 +1,4 @@
-var WIDTH, HEIGHT, scene, renderer, camera, cube;
+var WIDTH, HEIGHT, scene, renderer, camera, cube, plane;
 var stats;
 var keys = [];
 var cubes = [];
@@ -17,7 +17,7 @@ function setup() {
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(WIDTH, HEIGHT);
 
-  camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+  camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 100);
   camera.position.set(0, 3.5, 5);
   camera.lookAt(scene.position);
 
@@ -25,6 +25,31 @@ function setup() {
   defaultMaterial.color.setHex(myColor);
   cube = new THREE.Mesh(defaultGeometry, defaultMaterial);
   scene.add(cube);
+
+  var worldWidth = 100;
+  var worldDepth = 100;
+
+  data = generateHeight( worldWidth, worldDepth );
+
+  var geometry = new THREE.PlaneBufferGeometry( 7500, 7500, worldWidth - 1, worldDepth - 1 );
+				geometry.rotateX( - Math.PI / 2 );
+
+				var vertices = geometry.attributes.position.array;
+
+				for ( var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3 ) {
+
+					vertices[ j + 1 ] = data[ i ] * 1;
+
+				}
+
+				texture = new THREE.CanvasTexture( generateTexture( data, worldWidth, worldDepth ) );
+				texture.wrapS = THREE.ClampToEdgeWrapping;
+				texture.wrapT = THREE.ClampToEdgeWrapping;
+
+				mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial( { map: texture } ) );
+				scene.add( mesh );
+
+        camera.position.y = data[ 50 + 50 * 100 ] * 10 + 500;
 
   document.body.appendChild(renderer.domElement);
 
@@ -147,6 +172,9 @@ function broadcast(msg) {
 
 var counter = 0;
 
+var speed = 0;
+var MAX_SPEED = 1;
+
 function render() {
   window.requestAnimationFrame(render);
   if(counter % 20 === 0) {
@@ -157,19 +185,108 @@ function render() {
   stats.begin();
 
   if(keys[40]) {
-    cube.translateZ(0.05);
+    speed = speed + 0.005;
   }
   if(keys[38]) {
-    cube.translateZ(-0.05);
+    speed = speed - 0.005;
   }
   if(keys[39]) {
-    cube.translateX(0.05);
+    cube.rotateY(-(MAX_SPEED - speed) * 0.02);
   }
   if(keys[37]) {
-    cube.translateX(-0.05);
+    cube.rotateY((MAX_SPEED - speed) * 0.02);
   }
+
+  if(speed > MAX_SPEED)
+    speed = MAX_SPEED;
+
+  cube.translateZ(speed);
+
+  /*camera.position.x = cube.position.x;
+  camera.position.y = cube.position.y + 3.5;
+  camera.position.z = cube.position.z + 5;
+	camera.lookAt( cube.position );*/
 
   renderer.render(scene, camera);
 
   stats.end();
 }
+
+function generateHeight( width, height ) {
+	var size = width * height, data = new Uint8Array( size ),
+	perlin = new ImprovedNoise(), quality = 1, z = Math.random() * 100;
+	for ( var j = 0; j < 4; j ++ ) {
+		for ( var i = 0; i < size; i ++ ) {
+			var x = i % width, y = ~~ ( i / width );
+			data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * quality * 1.75 );
+		}
+		quality *= 5;
+	}
+	return data;
+}
+
+function generateTexture( data, width, height ) {
+
+				var canvas, canvasScaled, context, image, imageData,
+				level, diff, vector3, sun, shade;
+
+				vector3 = new THREE.Vector3( 0, 0, 0 );
+
+				sun = new THREE.Vector3( 1, 1, 1 );
+				sun.normalize();
+
+				canvas = document.createElement( 'canvas' );
+				canvas.width = width;
+				canvas.height = height;
+
+				context = canvas.getContext( '2d' );
+				context.fillStyle = '#000';
+				context.fillRect( 0, 0, width, height );
+
+				image = context.getImageData( 0, 0, canvas.width, canvas.height );
+				imageData = image.data;
+
+				for ( var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
+
+					vector3.x = data[ j - 2 ] - data[ j + 2 ];
+					vector3.y = 2;
+					vector3.z = data[ j - width * 2 ] - data[ j + width * 2 ];
+					vector3.normalize();
+
+					shade = vector3.dot( sun );
+
+					imageData[ i ] = ( 96 + shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
+					imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+					imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+				}
+
+				context.putImageData( image, 0, 0 );
+
+				// Scaled 4x
+
+				canvasScaled = document.createElement( 'canvas' );
+				canvasScaled.width = width * 4;
+				canvasScaled.height = height * 4;
+
+				context = canvasScaled.getContext( '2d' );
+				context.scale( 4, 4 );
+				context.drawImage( canvas, 0, 0 );
+
+				image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
+				imageData = image.data;
+
+				for ( var i = 0, l = imageData.length; i < l; i += 4 ) {
+
+					var v = ~~ ( Math.random() * 5 );
+
+					imageData[ i ] += v;
+					imageData[ i + 1 ] += v;
+					imageData[ i + 2 ] += v;
+
+				}
+
+				context.putImageData( image, 0, 0 );
+
+				return canvasScaled;
+
+			}
