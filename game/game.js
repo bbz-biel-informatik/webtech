76040ffabfb6c9e57c611099145266e6fa457c14
Lukas_Skywalker <lukas.diener@hotmail.com>
2017-03-23 17:58:46 +0100

    move js files

--- game/game.js
+++ game/game.js
@@ -1,320 +1,125 @@
-var WIDTH, HEIGHT, scene, renderer, camera, cube, plane, controls;
-var stats;
-var keys = [];
-var cubes = [];
-var checkpoints = [];
-var peer, myId, myName, myColor, peers, connections;
-var COLORS = [0xff0000, 0x0ff000, 0x00ff00, 0x000ff0, 0x0000ff];
-var CUBE_HEIGHT = 2;
+var jmp = false;
 
-var defaultMaterial = new THREE.MeshBasicMaterial({color: 0xFF0000});
-var defaultGeometry = new THREE.CubeGeometry(2, CUBE_HEIGHT, 2);
+function createEntity(element) {
+  element.css({position: 'absolute'});
+    return {
+        element: element,
+        game: null,
+        richtung: 0,
+        vs: 0,
+        geschwindigkeit: 1,
+        score: 0,
+        scoreSelector: '#random7438923749823',
+        name: '',
+        speed: function() { return this.geschwindigkeit; },
+        positionX: function() { return this.element.offset().left - this.game.left(); },
+        positionY: function() { return this.element.offset().top - this.game.top(); },
+        height: function() { return this.element.height(); },
+        width: function() { return this.element.width(); },
+        setPosition: function(x, y) { this.element.css({left: x + this.game.left(), top: y + this.game.top() }); },
+        jump: function(force) { this.vs = -4 * force; jmp = true; },
+        weightless: false,
+        colliding: function() {
+          for(var i = 0; i < this.game.entities.length; i++) {
+            var entity = this.game.entities[i];
+            if(this == entity) {
+              continue;
+            }
+            if(this.collision(entity)) {
+              return entity;
+            }
+          }
+          return false;
+        },
+        move: function() {
+          var left = this.positionX() + this.game.left() + cos2(this.richtung) * this.speed();
+          var top = this.positionY() + this.game.top() - sin2(this.richtung) * this.speed();
+          
+          if(this.game.gravity > 0 && this.weightless === false) {
+            var a = this.colliding();
+            if(a && !jmp) {
+              this.vs = 0;
+              top = a.element.position().top - this.element.height();
+            } else {
+              this.vs += 0.2 * this.game.gravity;
+              top += (this.vs * this.game.gravity);
+              jmp = false;
+            }
+          }
 
-var audio_enable = new Audio('enable.mp3');
-
-var clock = new THREE.Clock();
-
-function fullscreen() {
-  if (container.requestFullscreen) {
-    container.requestFullscreen();
-  } else if (container.msRequestFullscreen) {
-    container.msRequestFullscreen();
-  } else if (container.mozRequestFullScreen) {
-    container.mozRequestFullScreen();
-  } else if (container.webkitRequestFullscreen) {
-    container.webkitRequestFullscreen();
-  }
-}
-
-function setup() {
-  WIDTH  = window.innerWidth;
-  HEIGHT = window.innerHeight;
-
-  scene = new THREE.Scene();
-
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(WIDTH, HEIGHT);
-  element = renderer.domElement;
-
-  effect = new THREE.StereoEffect( renderer );
-  effect.eyeSeparation = 1;
-  effect.setSize( window.innerWidth, window.innerHeight );
-
-
-  camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 500);
-  camera.position.set(0, 2.5, 5);
-  camera.lookAt(scene.position);
-
-  controls = new THREE.OrbitControls(camera, element);
-  controls.target.set(
-    camera.position.x + 0.15,
-    camera.position.y,
-    camera.position.z
-  );
-  controls.noPan = true;
-  controls.noZoom = true;
-
-  function setOrientationControls(e) {
-    if (!e.alpha) {
-      return;
-    }
-
-    controls = new THREE.DeviceOrientationControls(camera, true);
-    controls.connect();
-    controls.update();
-
-    element.addEventListener('click', fullscreen, false);
-
-    window.removeEventListener('deviceorientation', setOrientationControls, true);
-  }
-  window.addEventListener('deviceorientation', setOrientationControls, true);
-
-  myColor = Math.random() * 0xffffff;
-  cube = new THREE.Mesh(defaultGeometry, defaultMaterial);
-  scene.add(cube);
-  cube.position.y = CUBE_HEIGHT / 2;
-  cube.add(camera);
-  setColor(cube, myColor);
-
-  var worldWidth = 100;
-  var worldDepth = 100;
-
-  for(var i = 0; i < 100; i++) {
-    var c = new THREE.Mesh(defaultGeometry, defaultMaterial);
-    c.scale.set(5, 5, 5);
-    setColor(c, Math.random() * 0xffffff);
-    c.position.set(-250 + Math.random() * 500, 2.5, -250 + Math.random() * 500);
-    scene.add(c);
-    checkpoints.push(c);
-  }
-
-  cube.lookAt(checkpoints[checkpoints.length - 1]);
-
-  //data = generateHeight( worldWidth, worldDepth );
-
-  var geometry = new THREE.PlaneBufferGeometry( 500, 500, worldWidth - 1, worldDepth - 1 );
-  geometry.rotateX( - Math.PI / 2 );
-
-  mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial({ wireframe: true }) );
-  scene.add( mesh );
-
-  document.body.appendChild(renderer.domElement);
-
-  document.addEventListener('keydown', function(event) {
-    keys[event.keyCode] = true;
-  });
-
-  document.addEventListener('keyup', function(event) {
-    keys[event.keyCode] = false;
-  });
-
-  // stats
-  stats = new Stats();
-  stats.setMode(0); // 0: fps, 1: ms, 2: mb
-
-  // align top-left
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.left = '0px';
-  stats.domElement.style.top = '0px';
-
-  document.body.appendChild( stats.domElement );
-
-  // peer, pw is asdfasdf
-  peer = new Peer({key: 'jzg83g5ozh8e61or'});
-
-  peers = [];
-  connections = [];
-
-  peer.on('open', function(id) {
-    var name = prompt('Name?');
-    myId = id;
-    myName = name;
-    displayText("Peer id is: " + id + "... ");
-
-    sendIdToServer(myId, name);
-  });
-
-  peer.on('connection', function(conn) {
-    console.log("Player entering the game: " + conn.peer);
-    var peerId = conn.peer;
-    peers.push(peerId);
-    connections.push(conn);
-    cubes[peerId] = createCube();
-    addPlayerToList(peerId, '');
-
-    conn.on('data', function(data) {
-      handleData(peerId, data);
-    });
-  });
-}
-
-function setdown() {
-  $.post("http://localhost:4567/goodbye", JSON.stringify({id: myId, name: myName}), null, 'json');
-}
-
-function createCube() {
-  var c = new THREE.Mesh(defaultGeometry, defaultMaterial);
-  scene.add(c);
-  return c;
-}
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-function setColor(obj, color) {
-  obj.material = new THREE.MeshBasicMaterial({color: color});
-}
-
-function displayText(text) {
-  var $status = $('#status');
-  $status.text($status.text() + "\n" + text);
-}
-
-function addPlayerToList(id, name) {
-  var $list = $('#players > ul');
-  var $item = $('<li id="' + id + '">' + name + '</li>');
-  $list.append($item);
-}
-
-function handleData(peerId, data) {
-  if(peerId === myId) {
-    return;
-  }
-  switch(data.type) {
-    case 'name':
-      var $player = $('li#' + peerId);
-      $player.text(data.type);
-      break;
-    case 'color':
-      setColor(cubes[peerId], data.value);
-      var $player = $('li#' + peerId);
-      var hashString = Math.round(data.value).toString(16);
-      $player.css('background-color', '#' + hashString);
-      break;
-    case 'position':
-      cubes[peerId].position.set(data.value[0], data.value[1], data.value[2]);
-      cubes[peerId].rotation.set(data.value[3], data.value[4], data.value[5]);
-      break;
-    default:
-      console.log(peerId + ': ' + data);
-  }
-}
-
-function sendIdToServer(id, name) {
-  jQuery.ajaxSetup({async:false});
-
-  $.post("http://localhost:4567/hello", JSON.stringify({id: id, name: name}), null, 'json');
-
-  displayText("Registered on server... ");
-
-  getIdsFromServer();
-}
-
-function getIdsFromServer() {
-  $.get( "http://localhost:4567/all", function( data ) {
-    for(var i = 0; i < data.length; i++) {
-      var id = data[i].id;
-      var name = data[i].name;
-      if(id != myId) {
-        console.log("Player already in room: " + name + " (" + id + ")");
-        peers.push(id);
-        addPlayerToList(id, name);
+          this.element.css({
+            left: left,
+            top: top
+          });
+        },
+      collision: function(b) {
+        return !(
+          ((this.positionY() + this.height()) < (b.positionY())) ||
+          (this.positionY() > (b.positionY() + b.height())) ||
+          ((this.positionX() + this.width()) < b.positionX()) ||
+          (this.positionX() > (b.positionX() + b.width()))
+        );
       }
-    }
-
-    for(var j = 0; j < peers.length; j++) {
-      var peer_id = peers[j];
-      var conn = peer.connect(peer_id);
-      connections.push(conn);
-      cubes[peer_id] = createCube();
-      (function(idCopy, connCopy) {
-        connCopy.on('data', function(data) {
-          handleData(idCopy, data);
-        });
-      }(id, conn));
-    }
-    //displayText("Got " + peers.length + " peers... ");
-    setColor(cube, myColor);
-    broadcast({type: 'color', value: myColor});
-  });
+    };
 }
 
-function broadcast(msg) {
-  for(var i = 0; i < connections.length; i++) {
-    connections[i].send(msg);
-  }
-}
-
-var counter = 0;
-
-var speed = 0.3;
-var MAX_SPEED = 1;
-
-function render() {
-  window.requestAnimationFrame(render);
-
-  var dt = clock.getDelta();
-
-  controls.update(dt);
-
-  if(counter % 10 === 0) {
-    broadcast({type: 'position', value: [cube.position.x, cube.position.y, cube.position.z, cube.rotation.x, cube.rotation.y, cube.rotation.z]});
-
-    if(counter % 100 === 0) {
-      broadcast({type: 'color', value: myColor});
-      broadcast({type: 'name', value: myName});
-    }
-  }
-  counter++;
-  stats.begin();
-
-  if(keys[40]) {
-    speed = speed - 0.005;
-  }
-  if(keys[38]) {
-    speed = speed + 0.005;
-  }
-  if(keys[39]) {
-    cube.rotateY(-(MAX_SPEED - speed * 0.5) * 0.02);
-  }
-  if(keys[37]) {
-    cube.rotateY((MAX_SPEED - speed * 0.5) * 0.02);
-  }
-
-  checkCollision();
-
-  if(speed > MAX_SPEED)
-    speed = MAX_SPEED;
-  if(speed < -MAX_SPEED)
-    speed = -MAX_SPEED;
-
-  cube.translateZ(-speed);
-
-  effect.render(scene, camera);
-
-  stats.end();
-}
-
-function checkCollision() {
-  for (var vertexIndex = 0; vertexIndex < cube.geometry.vertices.length; vertexIndex++) {
-    var localVertex = cube.geometry.vertices[vertexIndex].clone();
-    var globalVertex = localVertex.applyMatrix4(cube.matrix);
-    var directionVector = globalVertex.sub( cube.position );
-
-    var ray = new THREE.Raycaster( cube.position, directionVector.clone().normalize() );
-    var collisionResults = ray.intersectObjects( checkpoints );
-    if ( collisionResults.length > 0 && collisionResults.length < 5 && collisionResults[0].distance < directionVector.length() )
-    {
-        for(var i = 0; i < collisionResults.length; i++) {
-          setColor(collisionResults[i].object, myColor);
-          audio_enable.play();
+function createGame(element) {
+  return {
+    element: element,
+    entities: [],
+    add: function(entity) {
+      this.entities.push(entity);
+      entity.game = this;
+      this.element.append(entity.element);
+    },
+    remove: function(entity) {
+      entity.element.remove();
+      var index = this.entities.indexOf(entity);
+      if (index > -1) {
+        this.entities.splice(index, 1);
+      }
+    },
+    top: function() {
+      return this.element.offset().top;
+    },
+    left: function() {
+      return this.element.offset().left;
+    },
+    gravity: 0,
+      move: function() {
+        for(var i = 0; i < this.entities.length; i++) {
+          this.entities[i].move();
         }
-    }
-  }
+      },
+      updateScores: function() {
+        for(var i = 0; i < this.entities.length; i++) {
+          $(this.entities[i].scoreSelector).text(this.entities[i].score);
+        }
+      },
+      save: function() {
+        this.updateScores();
+        for(var i = 0; i < this.entities.length; i++) {
+          var name = this.entities[i].name;
+          var score = this.entities[i].score;
+          localStorage.setItem(name, score);
+        }
+      },
+      scores: function() {
+        var r = [];
+        for(var i = 0, len = localStorage.length; i < len; ++i ) {
+          var key = localStorage.key(i);
+          var val = localStorage.getItem(key);
+          r.push({name: key, score: val});
+        }
+        return r.sort(function(a, b){ return b.score - a.score; } );
+      }
+  };
+}
+
+function sin2(deg) {
+    return Math.sin(deg * (Math.PI / 180));
+}
+
+function cos2(deg) {
+    return Math.cos(deg * (Math.PI / 180));
 }
